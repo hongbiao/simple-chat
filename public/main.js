b9056438efce3bbd5ca341d963bc301c9452c34d
Hongbiao Chen <hongbiao_chen@apple.com>
2017-06-01 20:48:18 -0700

    use const

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 $(function() {
-  var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
-  var COLORS = [
+  const FADE_TIME = 150; // ms
+  const TYPING_TIMER_LENGTH = 400; // ms
+  const COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
